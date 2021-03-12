e6f6355d540c62fd96829879a4131d25cc7dc830
azhagusurriya <surriyaraj@gmail.com>
2021-03-11 21:46:13 -0500

    Deployed code

--- twitter-clone/src/Widgets.js
+++ twitter-clone/src/Widgets.js
@@ -18,17 +18,17 @@ function Widgets() {
       <div className="widgets__widgetContainer">
         <h2>What's happening</h2>
 
-        <TwitterTweetEmbed tweetId={"858551177860055040"} />
+        <TwitterTweetEmbed tweetId={"1367520948170027011"} />
 
         <TwitterTimelineEmbed
           sourceType="profile"
-          screenName="cleverqazi"
+          screenName="KingJames"
           options={{ height: 400 }}
         />
 
         <TwitterShareButton
-          url={"https://facebook.com/cleverprogrammer"}
-          options={{ text: "#reactjs is awesome", via: "cleverqazi" }}
+          url={"https://twitter.com/KingJames"}
+          options={{ text: "#Lebron is awesome", via: "cleverqazi" }}
         />
       </div>
     </div>
